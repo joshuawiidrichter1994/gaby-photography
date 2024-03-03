a0c7a862a703182c3de3b9a43b35bbf6cb29cf29
Joshua Richter <joshuawiidrichter@gmail.com>
2024-03-03 23:45:48 +0200

    Added new header

--- src/App.js
+++ src/App.js
@@ -1,7 +1,32 @@
 import './App.css';
 
 function App() {
-  return <div classNameName="App"></div>;
+  return (
+    <div classNameName="App">
+      <header>
+        <div className="icon-container">
+          <img className="icon" src={require('./img/logo.jpg')} />
+        </div>
+        <div className="nav-container">
+          <div className="nav-item">
+            <span>BLOG</span>
+          </div>
+          <div className="nav-item">
+            <span>WORKS</span>
+          </div>
+          <div className="nav-item">
+            <span>SERVICES</span>
+          </div>
+          <div className="nav-item">
+            <span>ABOUT</span>
+          </div>
+          <div className="nav-item">
+            <span>Get a Quote</span>
+          </div>
+        </div>
+      </header>
+    </div>
+  );
 }
 
 export default App;
