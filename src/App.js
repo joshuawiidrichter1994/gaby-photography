cda4727437e74017e7b60d32105eaa6e1dd8c11d
Joshua Richter <joshuawiidrichter@gmail.com>
2024-03-04 00:14:35 +0200

    Installed and integrated react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,36 +1,16 @@
 import './App.css';
+import Landing from './pages/Landing';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
-    <div classNameName="App">
-      <header>
-        <div className="icon-container">
-          <img className="icon" src={require('./img/logo.jpg')} />
-        </div>
-        <div className="nav-container">
-          <div className="nav-item">
-            <span>BLOG</span>
-          </div>
-          <div className="nav-item">
-            <span>WORKS</span>
-          </div>
-          <div className="nav-item">
-            <span>SERVICES</span>
-          </div>
-          <div className="nav-item">
-            <span>ABOUT</span>
-          </div>
-          <div className="nav-button">
-            <span>Get a Quote</span>
-          </div>
-        </div>
-      </header>
-      <main>
-        <div>
-          <h1>Gaby Isabelle Photography</h1>
-        </div>
-      </main>
-    </div>
+    <BrowserRouter>
+      <div classNameName="App">
+        <Routes>
+          <Route exact path="/" element={<Landing />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
---
+++ src/pages/Landing.js
@@ -0,0 +1,35 @@
+function Landing() {
+  return (
+    <div>
+      <header>
+        <div className="icon-container">
+          <img className="icon" src={require('../img/logo.jpg')} />
+        </div>
+        <div className="nav-container">
+          <div className="nav-item">
+            <span>BLOG</span>
+          </div>
+          <div className="nav-item">
+            <span>WORKS</span>
+          </div>
+          <div className="nav-item">
+            <span>SERVICES</span>
+          </div>
+          <div className="nav-item">
+            <span>ABOUT</span>
+          </div>
+          <div className="nav-button">
+            <span>Get a Quote</span>
+          </div>
+        </div>
+      </header>
+      <main>
+        <div>
+          <h1>Gaby Isabelle Photography</h1>
+        </div>
+      </main>
+    </div>
+  );
+}
+
+export default Landing;
