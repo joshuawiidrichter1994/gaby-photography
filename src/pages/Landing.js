6f60ef5d62e4259e9154b105acc751979e8e4af3
Joshua Richter <joshuawiidrichter@gmail.com>
2024-03-04 01:49:38 +0200

    Added blog section / Additional section styling

--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -32,7 +32,7 @@ function Landing() {
             <img className="intro-img" src={require('../img/1.JPG')} />
           </div>
         </div>
-        <div className="section">
+        <div className="portfolio-section">
           <div className="section-header">
             <span className="section-text-left">Portfolio</span>
             <span className="section-text-right">View Work</span>
@@ -40,12 +40,35 @@ function Landing() {
           <div className="section-images-container">
             <div className="section-image-container">
               <img className="section-image" src={require('../img/2.JPG')} />
+              <p className="section-image-text">Travel</p>
             </div>
             <div className="section-image-container">
               <img className="section-image" src={require('../img/3.jpg')} />
+              <p className="section-image-text">Brands</p>
             </div>
             <div className="section-image-container">
               <img className="section-image" src={require('../img/4.jpg')} />
+              <p className="section-image-text">Portraits</p>
+            </div>
+          </div>
+        </div>
+        <div className="blog-section">
+          <div className="section-header">
+            <span className="section-text-left">Blog</span>
+            <span className="section-text-right">View Work</span>
+          </div>
+          <div className="section-images-container">
+            <div className="section-image-container">
+              <img className="section-image" src={require('../img/7.jpg')} />
+              <p className="section-image-text">Photographing Pairs</p>
+            </div>
+            <div className="section-image-container">
+              <img className="section-image" src={require('../img/8.jpg')} />
+              <p className="section-image-text">Learning Light</p>
+            </div>
+            <div className="section-image-container">
+              <img className="section-image" src={require('../img/9.JPG')} />
+              <p className="section-image-text">Environmental Portraiture</p>
             </div>
           </div>
         </div>
