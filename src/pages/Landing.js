6a230cbaad913cb7461efd7df66f53afa7d84d7e
Joshua Richter <joshuawiidrichter@gmail.com>
2024-03-09 16:47:03 +0200

    Added mobile nav burger menu functionality and styling

--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -49,15 +49,64 @@ function Landing() {
           <img className="icon" src={require('../img/logo.jpg')} />
         </div>
 
-        <img
-          className="menu-icon"
-          onClick={toggleMobileNav}
-          src={require('../img/mobile-menu.png')}
-        />
-        <div className={isMobileNavVisible ? 'mobile-nav show' : 'mobile-nav'}>
-          <h1>HELLO</h1>
+        <div
+          className={
+            !isMobileNavVisible
+              ? 'mobile-nav-container show'
+              : 'mobile-nav-container'
+          }
+        >
+          <img
+            className="menu-icon"
+            onClick={toggleMobileNav}
+            src={require('../img/mobile-menu.png')}
+          />
+        </div>
+
+        <div
+          className={
+            isMobileNavVisible
+              ? 'mobile-nav-container show'
+              : 'mobile-nav-container'
+          }
+        >
+          <img
+            className="menu-icon"
+            onClick={toggleMobileNav}
+            src={require('../img/cross.png')}
+          />
         </div>
       </header>
+
+      <div
+        className={
+          isMobileNavVisible
+            ? 'mobile-nav-container show'
+            : 'mobile-nav-container'
+        }
+      >
+        <div className="mobile-nav">
+          <ul>
+            <li className="mobile-nav-item">
+              <span>BLOG</span>
+            </li>
+            <li className="mobile-nav-item">
+              <span>WORKS</span>
+            </li>
+            <li className="mobile-nav-item">
+              <span>SERVICES</span>
+            </li>
+            <li className="mobile-nav-item">
+              <span>ABOUT</span>
+            </li>
+          </ul>
+          <a href="https://www.google.com/" target="_blank">
+            <div className="mobile-nav-button">
+              <span>Get a Quote</span>
+            </div>
+          </a>
+        </div>
+      </div>
 
       {/* MAIN */}
       <main>
