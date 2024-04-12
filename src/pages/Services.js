a12d4930696f24358d2cf895e84c48f689920656
Joshua Richter <joshuawiidrichter@gmail.com>
2024-04-12 21:52:06 +0200

    Added and updated contact section placement in services

--- src/pages/Services.js
+++ src/pages/Services.js
@@ -16,32 +16,42 @@ function Services() {
                   className="grid-item-image"
                   src={require('../img/compressed/5.JPG')}
                 />
-                {/* <div className="grid-item-text">
-              <span>Branded Portraits</span>
-              <p>Don’t worry about sounding professional. Sound like you.</p>
-            </div> */}
+                <div className="grid-item-text">
+                  <span>Branded Portraits</span>
+                  <p>
+                    Don’t worry about sounding professional. Sound like you.
+                  </p>
+                </div>
               </div>
               <div className="grid-item">
                 <img
                   className="grid-item-image"
                   src={require('../img/compressed/7.jpg')}
                 />
-                {/* <div className="grid-item-text">
-              <span>Natural Lighting</span>
-              <p>Your goal should be to make it feel right for right now.</p>
-            </div> */}
+                <div className="grid-item-text">
+                  <span>Natural Lighting</span>
+                  <p>
+                    Your goal should be to make it feel right for right now.
+                  </p>
+                </div>
               </div>
               <div className="grid-item">
                 <img
                   className="grid-item-image"
                   src={require('../img/compressed/6.JPG')}
                 />
+                <div className="grid-item-text">
+                  <span>Natural Lighting</span>
+                  <p>
+                    Your goal should be to make it feel right for right now.
+                  </p>
+                </div>
               </div>
-              {/* <div className="contact-section">
-                <div className="contact-section-left"></div>
-                <div className="contact-section-right"></div>
-              </div> */}
             </div>
+          </div>
+          <div className="contact-section">
+            <div className="contact-section-left"></div>
+            <div className="contact-section-right"></div>
           </div>
         </div>
       </main>
