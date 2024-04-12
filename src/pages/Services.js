0b429279c4c0099d05ed21e977b7a5c43a06e6bb
Joshua Richter <joshuawiidrichter@gmail.com>
2024-04-12 21:48:16 +0200

    Fixed services grid spacing

--- src/pages/Services.js
+++ src/pages/Services.js
@@ -3,48 +3,48 @@ import './Services.css';
 
 function Services() {
   return (
-    <div className="services-container">
-      <div className="header-container">
-        <h1 className="header-text">Services</h1>
-      </div>
-      <div className="grid-container">
-        <div className="grid-row">
-          <div className="grid-item">
-            <img
-              className="grid-item-image"
-              src={require('../img/compressed/5.JPG')}
-            />
-            {/* <div className="grid-item-text">
+    <div>
+      <main>
+        <div className="services-container">
+          <div className="header-container">
+            <h1 className="header-text">Services</h1>
+          </div>
+          <div className="grid-container">
+            <div className="grid-row">
+              <div className="grid-item">
+                <img
+                  className="grid-item-image"
+                  src={require('../img/compressed/5.JPG')}
+                />
+                {/* <div className="grid-item-text">
               <span>Branded Portraits</span>
               <p>Don’t worry about sounding professional. Sound like you.</p>
             </div> */}
-          </div>
-          <div className="grid-item">
-            <img
-              className="grid-item-image"
-              src={require('../img/compressed/7.jpg')}
-            />
-            {/* <div className="grid-item-text">
+              </div>
+              <div className="grid-item">
+                <img
+                  className="grid-item-image"
+                  src={require('../img/compressed/7.jpg')}
+                />
+                {/* <div className="grid-item-text">
               <span>Natural Lighting</span>
               <p>Your goal should be to make it feel right for right now.</p>
             </div> */}
-          </div>
-          <div className="grid-item">
-            <img
-              className="grid-item-image"
-              src={require('../img/compressed/6.JPG')}
-            />
-            {/* <div className="grid-item-text">
-              <span>Groups</span>
-              <p>Be clear, be confident, and don’t overthink it.</p>
-            </div> */}
-          </div>
-          <div className="contact-section">
-            <div className="contact-section-left"></div>
-            <div className="contact-section-right"></div>
+              </div>
+              <div className="grid-item">
+                <img
+                  className="grid-item-image"
+                  src={require('../img/compressed/6.JPG')}
+                />
+              </div>
+              {/* <div className="contact-section">
+                <div className="contact-section-left"></div>
+                <div className="contact-section-right"></div>
+              </div> */}
+            </div>
           </div>
         </div>
-      </div>
+      </main>
     </div>
   );
 }
