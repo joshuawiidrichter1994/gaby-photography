67dc115f0fde30ebc6414e7e64449e075987172a
Joshua Richter <joshuawiidrichter@gmail.com>
2024-04-12 21:22:32 +0200

    Added grid item text for services

--- src/pages/Services.js
+++ src/pages/Services.js
@@ -14,19 +14,30 @@ function Services() {
               className="grid-item-image"
               src={require('../img/compressed/5.JPG')}
             />
-            <div className="grid-item-text"></div>
+            {/* <div className="grid-item-text">
+              <span>Branded Portraits</span>
+              <p>Don’t worry about sounding professional. Sound like you.</p>
+            </div> */}
           </div>
           <div className="grid-item">
             <img
               className="grid-item-image"
               src={require('../img/compressed/7.jpg')}
             />
+            {/* <div className="grid-item-text">
+              <span>Natural Lighting</span>
+              <p>Your goal should be to make it feel right for right now.</p>
+            </div> */}
           </div>
           <div className="grid-item">
             <img
               className="grid-item-image"
               src={require('../img/compressed/6.JPG')}
             />
+            {/* <div className="grid-item-text">
+              <span>Groups</span>
+              <p>Be clear, be confident, and don’t overthink it.</p>
+            </div> */}
           </div>
           <div className="contact-section">
             <div className="contact-section-left"></div>
