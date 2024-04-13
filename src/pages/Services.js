ecdff6084fce591cc589d243a15d907a7ab776cc
Joshua Richter <joshuawiidrichter@gmail.com>
2024-04-13 18:26:48 +0200

    Created contact form for services page

--- src/pages/Services.js
+++ src/pages/Services.js
@@ -57,7 +57,69 @@ function Services() {
                 Reach out to inquire about my services
               </span>
             </div>
-            <div className="contact-section-right"></div>
+            <div className="contact-section-right">
+              <form
+                className="contact-form"
+                action="mailto:info@example.com"
+                method="GET"
+              >
+                <span>name (required)</span>
+                <div className="contact-form-name-container">
+                  <div className="contact-form-name-item">
+                    <label>First Name</label>
+                    <br />
+                    <input
+                      type="text"
+                      name="first-name"
+                      className="contact-form-field-name"
+                      required
+                    ></input>
+                  </div>
+                  <div className="contact-form-name-item">
+                    <label>Last Name</label>
+                    <br />
+                    <input
+                      type="text"
+                      name="last-name"
+                      className="contact-form-field-name"
+                      required
+                    ></input>
+                  </div>
+                </div>
+                <div className="contact-form-email">
+                  <label>Email (required)</label>
+                  <br />
+                  <input
+                    type="text"
+                    name="email"
+                    className="contact-form-field"
+                    required
+                  ></input>
+                </div>
+                <div className="contact-form-phone">
+                  <label>Phone</label>
+                  <br />
+                  <input
+                    type="text"
+                    name="phone"
+                    className="contact-form-field"
+                  ></input>
+                </div>
+                <div className="contact-form-message">
+                  <label>Message (required)</label>
+                  <br />
+                  <input
+                    type="text"
+                    name="message"
+                    className="contact-form-field-message"
+                    required
+                  ></input>
+                </div>
+                <button type="submit" className="contact-form-submit-btn">
+                  Submit
+                </button>
+              </form>
+            </div>
           </div>
         </div>
       </main>
