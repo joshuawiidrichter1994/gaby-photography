3032b05b29da9cd844625469f9b77dc43c28284c
Joshua Richter <joshuawiidrichter@gmail.com>
2024-04-25 22:58:04 +0200

    Populated and styled blog page

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -52,7 +52,6 @@ const ContactForm = () => {
   return (
     <form className="contact-form" onSubmit={handleSubmit}>
       <div className="contact-form-name-container">
-        <span>Name (required):</span>
         <div className="contact-form-name-item">
           <label htmlFor="firstName">First Name</label>
           <input
--- src/pages/Blog.js
+++ src/pages/Blog.js
@@ -1,9 +1,78 @@
 import React from 'react';
+import './Blog.css';
 
 function Blog() {
   return (
-    <div>
-      <h1>BLOG</h1>
+    <div className="blogs-container">
+      <div className="header-container">
+        <h1 className="header-text">Blog</h1>
+      </div>
+      <div className="blog-grid">
+        <div className="blog-grid-row">
+          <div className="blog-container">
+            <img
+              className="blog-img"
+              src={require('../img/compressed/1.JPG')}
+            />
+            <div className="blog-info">
+              <span>25 April 2024</span>
+              <h2>The Beach</h2>
+              <p>
+                The Beach is a wonderful place, just wonderful. Lots of sand,
+                sun, and saltwater. Sounds like a jol
+              </p>
+              <a>read more...</a>
+            </div>
+          </div>
+          <div className="blog-container">
+            <img
+              className="blog-img"
+              src={require('../img/compressed/1.JPG')}
+            />
+            <div className="blog-info">
+              <span>25 April 2024</span>
+              <h2>The Beach</h2>
+              <p>
+                The Beach is a wonderful place, just wonderful. Lots of sand,
+                sun, and saltwater. Sounds like a jol
+              </p>
+              <a>read more...</a>
+            </div>
+          </div>
+        </div>
+        <div className="blog-grid-row">
+          <div className="blog-container">
+            <img
+              className="blog-img"
+              src={require('../img/compressed/1.JPG')}
+            />
+            <div className="blog-info">
+              <span>25 April 2024</span>
+              <h2>The Beach</h2>
+              <p>
+                The Beach is a wonderful place, just wonderful. Lots of sand,
+                sun, and saltwater. Sounds like a jol
+              </p>
+              <a>read more...</a>
+            </div>
+          </div>
+          <div className="blog-container">
+            <img
+              className="blog-img"
+              src={require('../img/compressed/1.JPG')}
+            />
+            <div className="blog-info">
+              <span>25 April 2024</span>
+              <h2>The Beach</h2>
+              <p>
+                The Beach is a wonderful place, just wonderful. Lots of sand,
+                sun, and saltwater. Sounds like a jol
+              </p>
+              <a>read more...</a>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
