5f8823175263dcf589521800cae9b4cd4ca513f6
Joshua Richter <joshuawiidrichter@gmail.com>
2024-05-18 19:26:42 +0200

    Added lazy load functionality

--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -1,7 +1,77 @@
-import React from 'react';
+import React, { useState } from 'react';
+import LazyLoad from 'react-lazyload';
 import './Portfolio.css';
 
+const imageList = [
+  require('../img/final/portfolio/1.jpg'),
+  require('../img/final/portfolio/2.jpg'),
+  require('../img/final/portfolio/3.jpg'),
+  require('../img/final/portfolio/4.jpg'),
+  require('../img/final/portfolio/5.jpg'),
+  require('../img/final/portfolio/6.jpg'),
+  require('../img/final/portfolio/7.jpg'),
+  require('../img/final/portfolio/8.jpg'),
+  require('../img/final/portfolio/9.jpg'),
+  require('../img/final/portfolio/10.jpg'),
+  require('../img/final/portfolio/11.jpg'),
+  require('../img/final/portfolio/12.jpg'),
+  require('../img/final/portfolio/13.jpg'),
+  require('../img/final/portfolio/14.jpg'),
+  require('../img/final/portfolio/15.jpg'),
+  require('../img/final/portfolio/16.jpg'),
+  require('../img/final/portfolio/17.JPG'),
+  require('../img/final/portfolio/18.JPG'),
+  require('../img/final/portfolio/19.JPG'),
+  require('../img/final/portfolio/20.JPG'),
+  require('../img/final/portfolio/21.JPG'),
+  require('../img/final/portfolio/22.JPG'),
+  require('../img/final/portfolio/23.JPG'),
+  require('../img/final/portfolio/24.JPG'),
+  require('../img/final/portfolio/25.JPG'),
+  require('../img/final/portfolio/26.JPG'),
+  require('../img/final/portfolio/27.JPG'),
+  require('../img/final/portfolio/28.JPG'),
+  require('../img/final/portfolio/29.JPG'),
+  require('../img/final/portfolio/30.JPG'),
+  require('../img/final/portfolio/31.JPG'),
+  require('../img/final/portfolio/32.JPG'),
+  require('../img/final/portfolio/33.JPG'),
+  require('../img/final/portfolio/34.JPG'),
+  require('../img/final/portfolio/35.JPG'),
+  require('../img/final/portfolio/36.JPG'),
+  require('../img/final/portfolio/37.JPG'),
+  require('../img/final/portfolio/38.JPG'),
+];
+
 function Portfolio() {
+  const [visibleCount, setVisibleCount] = useState(6);
+
+  const loadMore = () => {
+    setVisibleCount((prevCount) => prevCount + 6);
+  };
+
+  const createRows = (images) => {
+    const rows = [];
+    for (let i = 0; i < images.length; i += 2) {
+      rows.push(
+        <div className="grid-row" key={i}>
+          {images.slice(i, i + 2).map((src, index) => (
+            <div className="grid-item" key={index}>
+              <LazyLoad height={350} offset={100}>
+                <img
+                  className="grid-item-image"
+                  src={src}
+                  alt={`Portfolio ${i + index + 1}`}
+                />
+              </LazyLoad>
+            </div>
+          ))}
+        </div>
+      );
+    }
+    return rows;
+  };
+
   return (
     <div>
       <main>
@@ -10,49 +80,15 @@ function Portfolio() {
             <h1 className="header-text">Works</h1>
           </div>
           <div className="grid-container">
-            <div className="grid-row">
-              <div className="grid-item">
-                <img
-                  className="grid-item-image"
-                  src={require('../img/final/portfolio/1.jpg')}
-                />
-              </div>
-              <div className="grid-item">
-                <img
-                  className="grid-item-image"
-                  src={require('../img/final/portfolio/2.jpg')}
-                />
-              </div>
-            </div>
-            <div className="grid-row">
-              <div className="grid-item">
-                <img
-                  className="grid-item-image"
-                  src={require('../img/final/portfolio/3.jpg')}
-                />
-              </div>
-              <div className="grid-item">
-                <img
-                  className="grid-item-image"
-                  src={require('../img/final/portfolio/4.jpg')}
-                />
-              </div>
-            </div>
-            <div className="grid-row">
-              <div className="grid-item">
-                <img
-                  className="grid-item-image"
-                  src={require('../img/final/portfolio/5.jpg')}
-                />
-              </div>
-              <div className="grid-item">
-                <img
-                  className="grid-item-image"
-                  src={require('../img/final/portfolio/6.jpg')}
-                />
-              </div>
-            </div>
+            {createRows(imageList.slice(0, visibleCount))}
           </div>
+          {visibleCount < imageList.length && (
+            <div className="load-more-container">
+              <button onClick={loadMore} className="load-more-button">
+                Load More
+              </button>
+            </div>
+          )}
         </div>
       </main>
     </div>
