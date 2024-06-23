95fbb74ff62028b73f56df8f62c087ee43b7a21e
Joshua Richter <joshuawiidrichter@gmail.com>
2024-06-23 16:07:20 +0200

    Reversed CORS modification

--- backend/sendEmail.js
+++ backend/sendEmail.js
@@ -24,10 +24,7 @@ module.exports = async (req, res) => {
   console.log('Function invoked');
 
   // Set CORS headers to allow requests from specific origins
-  res.setHeader(
-    'Access-Control-Allow-Origin',
-    'https://gaby-photography.vercel.app'
-  );
+  res.setHeader('Access-Control-Allow-Origin', 'https://gaby-photography.com');
   res.setHeader('Access-Control-Allow-Methods', 'POST');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
 
