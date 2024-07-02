fec9bf1ad287ab8c8d63807b3dbe87f27b363607
Joshua Richter <joshuawiidrichter@gmail.com>
2024-07-02 18:01:16 +0200

    Added lottie with bubbles loading state

--- src/components/Spinner.js
+++ src/components/Spinner.js
@@ -1,8 +1,18 @@
 import React from 'react';
-import './Spinner.css';
+import { Player } from '@lottiefiles/react-lottie-player';
+import animationData from './lottie/bubbles.json';
 
 const Spinner = () => {
-  return <div className="spinner"></div>;
+  return (
+    <div className="lottie-container">
+      <Player
+        autoplay
+        loop
+        src={animationData}
+        style={{ height: '100px', width: '100px' }}
+      ></Player>
+    </div>
+  );
 };
 
 export default Spinner;
