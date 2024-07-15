f68c2a1df04d418c763920137d15b822b1f8017f
Joshua Richter <joshuawiidrichter@gmail.com>
2024-07-15 16:37:12 +0200

    Added text decoration for blog hyperlinks

--- src/pages/blogs/BlogTravel1.js
+++ src/pages/blogs/BlogTravel1.js
@@ -30,15 +30,29 @@ function BlogTravel1() {
         </h2>
         <p className="middle-section-text">
           During my stay, I had the pleasure of speaking with Louise, the
-          Marketing Manager, who shared insights about the hotel’s style and
-          decor. She described it as open, fresh, and breezy, with “spaces
-          within spaces.” I loved that phrase; it perfectly encapsulates the
-          hotel’s design philosophy. The architecture made the walls feel alive,
-          turning the hotel into a piece of art. The white, French-inspired
-          architecture pays homage to the hotel’s namesake, whose legacy is
-          woven into the fabric of the resort. Surrounded by these gorgeous
-          white buildings, lush gardens with tropical trees, and the endless
-          Mauritian coastline, I found myself constantly in awe.
+          Marketing Manager, who shared insights about the{' '}
+          <a
+            className="blog-link"
+            href="https://heritageresorts.mu/hotels-mauritius/le-telfair"
+            target="_blank"
+          >
+            hotel’s
+          </a>{' '}
+          style and decor. She described it as open, fresh, and breezy, with
+          “spaces within spaces.” I loved that phrase; it perfectly encapsulates
+          the hotel’s design philosophy. The architecture made the walls feel
+          alive, turning the hotel into a piece of art. The white,
+          French-inspired architecture pays homage to the{' '}
+          <a
+            className="blog-link"
+            href="https://heritageresorts.mu/hotels-mauritius/le-telfair"
+          >
+            hotel’s
+          </a>{' '}
+          namesake, whose legacy is woven into the fabric of the resort.
+          Surrounded by these gorgeous white buildings, lush gardens with
+          tropical trees, and the endless Mauritian coastline, I found myself
+          constantly in awe.
         </p>
         <div className="image-container">
           <img
@@ -50,14 +64,33 @@ function BlogTravel1() {
           Luxury Meets Warmth: The Essence of Serenity
         </h2>
         <p className="middle-section-text">
-          What struck me most about Heritage Le Telfair was its ability to be
-          both elegant and homely. The staff were genuinely kind, and the decor
-          and architecture were refined without being over-the-top,
-          complementing the natural scenery. The Heritage Resorts span across
-          Bel Ombre, a UNESCO World Heritage site in the South of Mauritius. The
-          untouched forests and carefully developed areas create a harmonious
-          blend of nature and luxury, with stunning hotels and 12 restaurants.
-          Despite the development, nothing feels congested or cluttered;
+          What struck me most about{' '}
+          <a
+            className="blog-link"
+            href="https://heritageresorts.mu/hotels-mauritius/le-telfair"
+            target="_blank"
+          ></a>
+          Heritage Le Telfair was its ability to be both elegant and homely. The
+          staff were genuinely kind, and the decor and architecture were refined
+          without being over-the-top, complementing the natural scenery.{' '}
+          <a
+            className="blog-link"
+            href="https://heritageresorts.mu/hotels-mauritius/le-telfair"
+            target="_blank"
+          >
+            The Heritage Resorts
+          </a>{' '}
+          span across Bel Ombre, a UNESCO World Heritage site in the South of
+          Mauritius. The untouched forests and carefully developed areas create
+          a harmonious blend of nature and luxury, with stunning hotels and{' '}
+          <a
+            className="blog-link"
+            href="https://heritageresorts.mu/hotels-mauritius/le-telfair/restaurants-bars"
+            target="_blank"
+          >
+            12 restaurants
+          </a>
+          . Despite the development, nothing feels congested or cluttered;
           everything is perfectly placed.
         </p>
         <div className="image-container">
@@ -72,11 +105,26 @@ function BlogTravel1() {
         </h2>
         <p className="middle-section-text">
           We visited the restaurants, each with its own niche and signature
-          style. The activities, including golf, museum tours, workshops, and
-          jazz evenings, added depth to the overall experience. I appreciated
-          how each resort and restaurant kept its distinctive style while
-          seamlessly connecting as a brand through their marketing, interiors,
-          and, in the case of the restaurants, the food.
+          style. The{' '}
+          <a
+            className="blog-link"
+            href="https://heritageresorts.mu/hotels-mauritius/le-telfair/things-to-do"
+            target="_blank"
+          >
+            activities
+          </a>
+          , including golf, museum tours, workshops, and jazz evenings, added
+          depth to the overall experience. I appreciated how each resort and{' '}
+          <a
+            className="blog-link"
+            href="https://heritageresorts.mu/hotels-mauritius/le-telfair/restaurants-bars"
+            target="_blank"
+          >
+            restaurant
+          </a>{' '}
+          kept its distinctive style while seamlessly connecting as a brand
+          through their marketing, interiors, and, in the case of the
+          restaurants, the food.
         </p>
         <div className="image-container">
           <img
@@ -91,13 +139,35 @@ function BlogTravel1() {
           From the scenic views to the intricate decor, the elegant
           architecture, and the exquisite cuisine, the entire experience was a
           work of art. It was creative, moving, and inspiring, constantly
-          blowing me away. This journey to Mauritius and Heritage Le Telfair was
-          more than just a vacation; it was an experience that left me inspired
-          and longing for more.
+          blowing me away. This journey to Mauritius and{' '}
+          <a
+            className="blog-link"
+            href="https://heritageresorts.mu/hotels-mauritius/le-telfair"
+            target="_blank"
+          >
+            Heritage Le Telfair
+          </a>{' '}
+          was more than just a vacation; it was an experience that left me
+          inspired and longing for more.
         </p>
         <p className="learn-more-text">
-          *To learn more, head to their website at Heritage Le Telfair or book
-          directly online.{' '}
+          *To learn more, head to their website at{' '}
+          <a
+            className="blog-link"
+            href="https://heritageresorts.mu/hotels-mauritius/le-telfair"
+            target="_blank"
+          >
+            Heritage Le Telfair
+          </a>{' '}
+          or{' '}
+          <a
+            className="blog-link"
+            href="https://reservations.heritageresorts.mu/?hotel=59976&chain=14463&themeCode=TELFAIRTHEME&configCode=new_BE&locale=en-US&adult=2&promo=Loyalty01&nights=2"
+            target="_blank"
+          >
+            book directly
+          </a>{' '}
+          online.
         </p>
       </section>
     </div>
