048554e59446e9d97f8a2ec49b4fc36576378dfa
Joshua Richter <joshuawiidrichter@gmail.com>
2024-07-15 13:05:52 +0200

    Created custom blog page

--- src/pages/blogs/BlogDefault.js
+++ src/pages/blogs/BlogDefault.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import './Blog.css';
+import './BlogDefault.css';
 
 function Blog() {
   return (
---
+++ src/pages/blogs/BlogTravel1.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Blog.css';
+
+function BlogTravel1() {
+  return (
+    <div>
+      <section></section>
+      <section></section>
+    </div>
+  );
+}
+
+export default BlogTravel1;
